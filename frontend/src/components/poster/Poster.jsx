383678d53ced3439c56aba2d467539fff7326c40
JulienOza <julien.ozarowski@gmail.com>
2023-10-25 16:43:51 +0200

    create components PageQuiz and Poster

---
+++ frontend/src/components/poster/Poster.jsx
@@ -0,0 +1,13 @@
+import "./Poster.css";
+
+function Poster() {
+  return (
+    <img
+      className="poster__img"
+      src="https://i.ebayimg.com/images/g/HDwAAOSwFJZkvjiG/s-l1600.jpg"
+      alt="affiche de film"
+    />
+  );
+}
+
+export default Poster;
---
+++ frontend/src/components/quiz-page/QuizPage.jsx
@@ -0,0 +1,8 @@
+import "./QuizPage.css";
+import Poster from "../poster/Poster";
+
+function QuizPage() {
+  return <Poster />;
+}
+
+export default QuizPage;
