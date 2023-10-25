20c5847fb10d19adbe130b0d95b44c3d3f34275a
Rayane0823 <rayanemoussaoui08@gmail.com>
2023-10-25 16:31:55 +0200

    Adding the Clues component

---
+++ frontend/src/assets/components/Indices/Clues.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Clues.css";
+
+function Indices() {
+  return (
+    <>
+      <p className="Clues" id="Date-clue">
+        1993
+      </p>
+      <p className="Clues" id="Producer-clue">
+        Steven Spielberg
+      </p>
+      <p className="Clues" id="Genre-clue">
+        Science-fiction / Aventure
+      </p>
+      <p className="Clues" id="Casting-clue">
+        Sam Neill / Laura Dern
+      </p>
+    </>
+  );
+}
+
+export default Indices;
