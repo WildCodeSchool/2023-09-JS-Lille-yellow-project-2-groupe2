0d3bc1604fc951e71a3fe1fec621179c04d84012
SebSamPro <sebastien.samyn.pro@gmail.com>
2023-10-25 17:03:10 +0200

    add component homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,8 @@
-import Counter from "./components/Counter";
-import logo from "./assets/logo.svg";
-
 import "./App.css";
+import Home from "./components/home-folder/home";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome on your P2!</p>
-
-        <Counter />
-
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
---
+++ frontend/src/components/home-folder/home.jsx
@@ -0,0 +1,29 @@
+import "./home.css";
+import logo from "../../assets/logo.png";
+
+function Home() {
+  return (
+    <section className="all-elements-home">
+      <img
+        src={logo}
+        className="logo-home"
+        alt="logo jaune ciné wild quiz de la page d'accueil"
+        href="#"
+      />
+      ;<p className="slogan">Vous croyez tout connaître du Cinéma !</p>;
+      <section className="btn-allButtons">
+        <button type="button" className="homeButton">
+          Jouer
+        </button>
+        <button type="button" className="homeButton">
+          Classement
+        </button>
+        <button type="button" className="homeButton">
+          Casting
+        </button>
+      </section>
+    </section>
+  );
+}
+
+export default Home;
