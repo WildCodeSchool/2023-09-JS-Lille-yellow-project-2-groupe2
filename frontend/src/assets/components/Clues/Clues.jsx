486993dbfd6ed0350c843eb5c895abb4913f3713
Rayane0823 <rayanemoussaoui08@gmail.com>
2023-10-26 11:06:35 +0200

    requested changes made

--- frontend/src/assets/components/Clues/Clues.jsx
+++ frontend/src/assets/components/Clues/Clues.jsx
@@ -4,18 +4,10 @@ import "./Clues.css";
 function Clues() {
   return (
     <>
-      <p className="Clues" id="Date-clue">
-        1993
-      </p>
-      <p className="Clues" id="Producer-clue">
-        Steven Spielberg
-      </p>
-      <p className="Clues" id="Genre-clue">
-        Science-fiction / Aventure
-      </p>
-      <p className="Clues" id="Casting-clue">
-        Sam Neill / Laura Dern
-      </p>
+      <p className="Clues Date-clue">1993</p>
+      <p className="Clues Producer-clue">Steven Spielberg</p>
+      <p className="Clues Genre-clue">Science-fiction / Aventure</p>
+      <p className="Clues Casting-clue">Sam Neill / Laura Dern</p>
     </>
   );
 }
