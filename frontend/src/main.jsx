a1387063876f37a956878891964455ed4b7ee126
Rayane0823 <rayanemoussaoui08@gmail.com>
2023-10-27 10:00:50 +0200

    Add Quizpage into router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,16 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
+import QuizPage from "./components/quiz-page/QuizPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/quiz",
+    element: <QuizPage />,
   },
 ]);
 
