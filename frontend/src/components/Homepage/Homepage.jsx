f965024476c69671fa0430712fe6dd71a409c29b
Rayane0823 <rayanemoussaoui08@gmail.com>
2023-10-31 16:43:14 +0100

    Correcting linter issue

--- frontend/src/components/Homepage/Homepage.jsx
+++ frontend/src/components/Homepage/Homepage.jsx
@@ -8,32 +8,34 @@ function Home() {
   const [openingPopUp, setOpeningPopUp] = useState(false);
 
   return (
-    <section
-      className={`all-elements-home ${openingPopUp ? "OpenedPopUp" : ""}`}
-    >
-      <img
-        src={logo}
-        className="logo-home"
-        alt="logo jaune ciné wild quiz de la page d'accueil"
-      />
-      ;<p className="slogan">Vous croyez tout connaître du Cinéma !</p>;
-      <section className="btn-allButtons">
-        <button
-          type="button"
-          className="homeButton"
-          onClick={() => setOpeningPopUp(true)}
-        >
-          Jouer
-        </button>
-        {openingPopUp && <PopUp setOpeningPopUp={setOpeningPopUp} />}
-        <Link to="/Ranking" className="homeButton">
-          Classement
-        </Link>
-        <Link to="/Casting" className="homeButton">
-          Casting
-        </Link>
+    <main>
+      <section
+        className={`all-elements-home ${openingPopUp ? "OpenedPopUp" : ""}`}
+      >
+        <img
+          src={logo}
+          className="logo-home"
+          alt="logo jaune ciné wild quiz de la page d'accueil"
+        />
+        <p className="slogan">Vous croyez tout connaître du Cinéma !</p>
+        <section className="btn-allButtons">
+          <button
+            type="button"
+            className="homeButton"
+            onClick={() => setOpeningPopUp(true)}
+          >
+            Jouer
+          </button>
+          {openingPopUp && <PopUp setOpeningPopUp={setOpeningPopUp} />}
+          <Link to="/Ranking" className="homeButton">
+            Classement
+          </Link>
+          <Link to="/Casting" className="homeButton">
+            Casting
+          </Link>
+        </section>
       </section>
-    </section>
+    </main>
   );
 }
 
