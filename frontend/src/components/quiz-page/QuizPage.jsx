28fa166a172fbd2b3ab99a734f142b2d362d5d1b
SebSamPro <sebastien.samyn.pro@gmail.com>
2023-10-31 12:01:17 +0100

    add timer on quizpage

--- frontend/src/components/quiz-page/QuizPage.jsx
+++ frontend/src/components/quiz-page/QuizPage.jsx
@@ -2,12 +2,14 @@ import Poster from "../poster/Poster";
 import Clues from "../Clues/Clues";
 import Input from "../Input/Input";
 import "./QuizPage.css";
+import TimerApp from "../TimerApp/TimerApp";
 
 function QuizPage() {
   return (
     <section className="quizpage_container">
       <Poster />
       <Clues />
+      <TimerApp />
       <Input movieTitle="Jurassic Park" />
     </section>
   );
