7bc093f08ee9dc949347169b50034352b60ec4b4
SebSamPro <sebastien.samyn.pro@gmail.com>
2023-11-15 14:41:41 +0100

    Add API

--- frontend/src/components/Clues/Clues.jsx
+++ frontend/src/components/Clues/Clues.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import PropTypes from "prop-types";
 import "./Clues.css";
 
-function Clues() {
+function Clues({ movieDate, movieDirector, movieGenres, movieCast }) {
+  const movieYear = movieDate.slice(0, 4);
+
   return (
     <section className="clues">
-      <p className="date-clue">1993</p>
-      <p className="producer-clue">Steven Spielberg</p>
-      <p className="genre-clue">Science-fiction / Aventure</p>
-      <p className="casting-clue">Sam Neill / Laura Dern</p>
+      <p className="date-clue">{movieYear}</p>
+      <p className="producer-clue">{movieDirector}</p>
+      <p className="genre-clue">{movieGenres}</p>
+      <p className="casting-clue">{movieCast}</p>
     </section>
   );
 }
+
+Clues.propTypes = {
+  movieDate: PropTypes.string.isRequired,
+  movieDirector: PropTypes.string.isRequired,
+  movieGenres: PropTypes.string.isRequired,
+  movieCast: PropTypes.string.isRequired,
+};
 
 export default Clues;
--- frontend/src/components/quiz-page/QuizPage.jsx
+++ frontend/src/components/quiz-page/QuizPage.jsx
@@ -5,7 +5,6 @@ import Clues from "../Clues/Clues";
 import Input from "../Input/Input";
 import ResultPopUp from "../ResultPopUp/ResultPopUp";
 // Movie list that will be replaced by API calls
-import movies from "../movies";
 import "./QuizPage.css";
 
 function QuizPage() {
@@ -17,6 +16,77 @@ function QuizPage() {
   const [gameOver, setGameOver] = useState(false);
   // User score
   const [score, setScore] = useState(0);
+  const [movie, setMovie] = useState(null);
+  const [movieCast, setMovieCast] = useState([]);
+  const [movieDirector, setMovieDirector] = useState();
+  const [movieGenres, setMovieGenres] = useState();
+
+  function getGenre(genreList, movieGenre) {
+    const genres = genreList.filter((genre) => movieGenre.includes(genre.id));
+    return genres;
+  }
+
+  useEffect(() => {
+    const randomMovie = Math.floor(Math.random() * 19);
+    const randomPage = Math.floor(Math.random() * 39) + 1;
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1OTAzZGExODJkODUyNWQ2YzBiYTZlNzNiY2Q1YjA4MSIsInN1YiI6IjY1NGI1NWQ2NDFhNTYxMzM2ZDg2NjA3OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3C9MzFQ-cGmIQFXHUAvhGaJRqQXR3suZ9u-aApF70R8",
+      },
+    };
+
+    fetch(
+      `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=fr-FR&page=${randomPage}&sort_by=popularity.desc&vote_count.gte=5000`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        setMovie(response.results[randomMovie]);
+      })
+      .catch((err) => console.error(err));
+  }, [questionIndex]);
+
+  useEffect(() => {
+    if (movie) {
+      const options = {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1OTAzZGExODJkODUyNWQ2YzBiYTZlNzNiY2Q1YjA4MSIsInN1YiI6IjY1NGI1NWQ2NDFhNTYxMzM2ZDg2NjA3OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3C9MzFQ-cGmIQFXHUAvhGaJRqQXR3suZ9u-aApF70R8",
+        },
+      };
+      fetch(
+        `https://api.themoviedb.org/3/movie/${movie.id}/credits?language=en-US`,
+        options
+      )
+        .then((response) => response.json())
+        .then((response) => {
+          const movieActors = [];
+          movieActors.push(response.cast[0].name);
+          movieActors.push(response.cast[1].name);
+          setMovieCast(movieActors);
+          setMovieDirector(
+            response.crew.filter((crew) => crew.job === "Director")
+          );
+        })
+        .catch((err) => console.error(err));
+
+      fetch(
+        "https://api.themoviedb.org/3/genre/movie/list?language=fr-FR",
+        options
+      )
+        .then((response) => response.json())
+        .then((response) => {
+          const movieCategory = getGenre(response.genres, movie.genre_ids);
+          setMovieGenres(movieCategory);
+        })
+        .catch((err) => console.error(err));
+    }
+  }, [movie]);
 
   // Resets questionOver state on question switch
   useEffect(() => {
@@ -24,34 +94,43 @@ function QuizPage() {
   }, [questionIndex]);
 
   return (
-    <section className="quizpage_container">
-      <Navbar
-        gameOver={gameOver}
-        setGameOver={setGameOver}
-        questionIndex={questionIndex}
-        questionOver={questionOver}
-        setQuestionOver={setQuestionOver}
-      />
-      <Poster poster={movies[questionIndex - 1].poster_path} />
-      <Clues />
-      <Input
-        movieTitle={movies[questionIndex - 1].title}
-        setGameOver={setGameOver}
-        score={score}
-        setScore={setScore}
-        questionIndex={questionIndex}
-        questionOver={questionOver}
-        setQuestionOver={setQuestionOver}
-      />
-      <ResultPopUp
-        gameOver={gameOver}
-        score={score}
-        questionIndex={questionIndex}
-        setQuestionIndex={setQuestionIndex}
-        questionOver={questionOver}
-        setQuestionOver={setQuestionOver}
-      />
-    </section>
+    movie && (
+      <section className="quizpage_container">
+        <Navbar
+          gameOver={gameOver}
+          setGameOver={setGameOver}
+          questionIndex={questionIndex}
+          questionOver={questionOver}
+          setQuestionOver={setQuestionOver}
+        />
+        <Poster poster={movie.poster_path} />
+        <Clues
+          movieDate={movie.release_date}
+          movieDirector={movieDirector && movieDirector[0].name}
+          movieGenres={
+            movieGenres && `${movieGenres[0].name} / ${movieGenres[1].name}`
+          }
+          movieCast={movieCast && `${movieCast[0]} / ${movieCast[1]}`}
+        />
+        <Input
+          movieTitle={movie.title}
+          setGameOver={setGameOver}
+          score={score}
+          setScore={setScore}
+          questionIndex={questionIndex}
+          questionOver={questionOver}
+          setQuestionOver={setQuestionOver}
+        />
+        <ResultPopUp
+          gameOver={gameOver}
+          score={score}
+          questionIndex={questionIndex}
+          setQuestionIndex={setQuestionIndex}
+          questionOver={questionOver}
+          setQuestionOver={setQuestionOver}
+        />
+      </section>
+    )
   );
 }
 
